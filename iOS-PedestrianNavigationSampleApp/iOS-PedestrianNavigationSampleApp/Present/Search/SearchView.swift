a5e2f3f127d2eb1907a1f171738e8d8c59240343
profit <profit0124@gmail.com>
2024-09-12 10:10:59 +0900

    Project basic setting

---
+++ iOS-PedestrianNavigationSampleApp/iOS-PedestrianNavigationSampleApp/Present/Search/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  iOS-PedestrianNavigationSampleApp
+//
+//  Created by Sooik Kim on 9/12/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("Search View")
+    }
+}
+
+#Preview {
+    SearchView()
+}
