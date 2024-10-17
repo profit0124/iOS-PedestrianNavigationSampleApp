6f98d249643640568b9139bb4d319267b6825e3f
profit <profit0124@gmail.com>
2024-10-17 16:53:34 +0900

    [feature] #16 Tmap 경로 동시 확인

--- iOS-PedestrianNavigationSampleApp/iOS-PedestrianNavigationSampleApp/Present/Mapbox/MapboxNavigationView.swift
+++ iOS-PedestrianNavigationSampleApp/iOS-PedestrianNavigationSampleApp/Present/Mapbox/MapboxNavigationView.swift
@@ -7,6 +7,17 @@
 
 import SwiftUI
 import MapKit
+import MapboxDirections
+
+extension ProfileIdentifier {
+    func getImage() -> Image {
+        switch self {
+        case .walking: return Image(systemName: "figure.walk")
+        case .cycling: return Image(systemName: "bicycle")
+        default: return Image(systemName: "person")
+        }
+    }
+}
 
 struct MapboxNavigationView: View {
     
@@ -16,16 +27,44 @@ struct MapboxNavigationView: View {
         self._vm = .init(wrappedValue: .init(model: model))
     }
     
+    let searchTypeList: [ProfileIdentifier] = [.walking, .cycling]
+    
     var body: some View {
         ZStack {
             MapboxNavigationViewRepresentable(vm: vm)
             
+            VStack {
+                
+                Spacer()
+                
+                Button {
+                    vm.send(.onAppear)
+                } label: {
+                    Text("경로 재탐색")
+                        .font(.system(size: 16, weight: .semibold))
+                        .foregroundStyle(.white)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background {
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(.blue)
+                        }
+                }
+                .padding(.horizontal, 16)
+                .padding(.bottom, 16)
+            }
+        }
+        .overlay {
             if !vm.isCompleted {
                 ProgressView()
             }
         }
         .onAppear {
             vm.send(.onAppear)
+            UIApplication.shared.isIdleTimerDisabled = true
+        }
+        .onDisappear{
+            UIApplication.shared.isIdleTimerDisabled = false
         }
     }
 }
@@ -53,7 +92,11 @@ struct MapboxNavigationViewRepresentable: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
-        context.coordinator.addPolyLine()
+        if vm.isCompleted {
+            context.coordinator.addPolyLine()
+        } else {
+            context.coordinator.deletePolyLine()
+        }
     }
     
     final class Coordinator: NSObject, MKMapViewDelegate {
@@ -64,23 +107,47 @@ struct MapboxNavigationViewRepresentable: UIViewRepresentable {
         }
         
         func addPolyLine() {
-            let coordinator = parent.vm.mapboxCoordinates
-            let polyLine = MKPolyline(coordinates: coordinator, count: coordinator.count)
+            if let mapboxCoordinates = parent.vm.mapboxCoordinates {
+                let polyLine = CustomPolyline(coordinates: mapboxCoordinates, count: mapboxCoordinates.count)
+                polyLine.identifier = "Mapbox"
+                parent.mapView.addOverlay(polyLine)
+            }
             
-            parent.mapView.addOverlay(polyLine)
+            if let tmapCoordinates = parent.vm.tmapCoordinates {
+                let polyLine = CustomPolyline(coordinates: tmapCoordinates, count: tmapCoordinates.count)
+                polyLine.identifier = "tmap"
+                parent.mapView.addOverlay(polyLine)
+            }
+        }
+        
+        func deletePolyLine() {
+            parent.mapView.removeOverlays(parent.mapView.overlays)
+        }
+        
+        func updatePolyLine() {
+            deletePolyLine()
+            addPolyLine()
         }
         
         func mapView(_ mapView: MKMapView, rendererFor overlay: any MKOverlay) -> MKOverlayRenderer {
             switch overlay {
-                case let polyline as MKPolyline:
+            case let polyline as CustomPolyline:
                 let renderer = MKPolylineRenderer(overlay: polyline)
-                renderer.strokeColor = .red
-                renderer.lineWidth = 3
+                if polyline.identifier == "Mapbox" {
+                    renderer.strokeColor = .blue
+                } else {
+                    renderer.strokeColor = .red
+                }
+                renderer.lineWidth = 5
                 return renderer
                 
             default:
-                fatalError()
+                return MKOverlayRenderer(overlay: overlay)
             }
         }
     }
+}
+
+class CustomPolyline: MKPolyline {
+    var identifier: String?
 }
--- iOS-PedestrianNavigationSampleApp/iOS-PedestrianNavigationSampleApp/Present/Mapbox/MapboxNavigationViewModel.swift
+++ iOS-PedestrianNavigationSampleApp/iOS-PedestrianNavigationSampleApp/Present/Mapbox/MapboxNavigationViewModel.swift
@@ -13,17 +13,23 @@ import MapboxCoreNavigation
 
 final class MapboxNavigationViewModel: ObservableObject {
     let searchModel: SearchResultModel
-    let mapboxService: MapboxServiceType
+    private let mapboxService: MapboxServiceType
+    private let routeService: RoutesService
     
     var cancellable = Set<AnyCancellable>()
     
     @Published var isCompleted: Bool = false
-    var mapboxCoordinates: [CLLocationCoordinate2D] = []
+    var mapboxCoordinates: [CLLocationCoordinate2D]?
+    private var tmapModel: [NavigationModel]?
     
+    var tmapCoordinates: [CLLocationCoordinate2D]? {
+        return tmapModel?.flatMap { $0.lineModels }.flatMap { $0.cooridnates }
+    }
     
     init(model: SearchResultModel) {
         self.searchModel = model
         self.mapboxService = MapboxService()
+        self.routeService = RoutesService()
     }
     
     enum Action {
@@ -34,11 +40,13 @@ final class MapboxNavigationViewModel: ObservableObject {
         switch action {
         case .onAppear:
             fetchMapboxDirection()
+            fetchTmapDirection()
         }
     }
     
     func fetchMapboxDirection() {
         isCompleted = false
+        self.mapboxCoordinates = nil
         mapboxService.fetch(CLLocationCoordinate2D(latitude: searchModel.lat, longitude: searchModel.long), profiler: .walking)
             .receive(on: DispatchQueue.main)
             .sink(receiveCompletion: {
@@ -46,8 +54,32 @@ final class MapboxNavigationViewModel: ObservableObject {
             }, receiveValue: { [weak self] coordinates in
                 guard let self = self else { return }
                 self.mapboxCoordinates = coordinates
-                self.isCompleted = true
+                self.checkIsCompleted()
             })
             .store(in: &cancellable)
+    }
+    
+    func fetchTmapDirection() {
+        isCompleted = false
+        self.tmapModel = nil
+        routeService.fetch(searchModel)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                if case .failure(let error) = completion {
+                    print(error)
+                }
+            }, receiveValue: { [weak self] result in
+                guard let self = self else { return }
+                self.tmapModel = result.routes
+                self.checkIsCompleted()
+                
+            })
+            .store(in: &cancellable)
+    }
+    
+    func checkIsCompleted() {
+        if tmapModel != nil && mapboxCoordinates != nil {
+            isCompleted = true
+        }
     }
 }
